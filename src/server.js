5725022462efaaed700ef6bf0b4396cb4334282c
Gguife <gguife747@gmail.com>
2024-02-16 19:47:23 -0300

    feat: Initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/health", (_, res) =>{
+  return res.send("Hello World!")
+})
+
+app.listen(8080, async ()=>{
+  console.log("Server runing on port 8080")
+})
