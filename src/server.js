56b55e7718bb8102d312e5ee8f5aa986f0d64057
Gguife <gguife747@gmail.com>
2024-02-25 21:29:33 -0300

    feat: Connected with DB

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import express from "express";
 import cors from "cors";
-
+import db from "./config/database.js";
 
 const app = express();
 app.use(cors());
 
-
+db.sync()
+  .then(() => {
+    console.log(`DB is connected: ${process.env.DB_NAME}`);
+  })
+  .catch(error => {
+    console.error('Unable to connect to the database:', error);
+  });
+  
 app.get("/health", (_, res) =>{
   return res.send("Hello World!")
 })
 
 app.listen(8080, async ()=>{
-  console.log("Server runing on port 8080")
+  console.log("Server is running on port 8080")
 })
