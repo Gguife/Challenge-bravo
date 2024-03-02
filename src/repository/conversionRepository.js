7f1399ba01598551cdf9455d0a0d28401c3c1f01
Gguife <gguife747@gmail.com>
2024-03-02 15:16:29 -0300

    feat: Created conversion coin repository and service

---
+++ src/repository/conversionRepository.js
@@ -0,0 +1,2 @@
+// Vamos fazer contato direto com o banco de dados, manipulando, utlizando Sequelize
+// Vamos fazer as operações de conversao da moeda
---
+++ src/service/conversionService.js
@@ -0,0 +1,3 @@
+// Ficara contido a Lógica de negçocio
+// Iremos encapsular os dados enviados para conversão
+// Iremos retornar o resultado da conversão
