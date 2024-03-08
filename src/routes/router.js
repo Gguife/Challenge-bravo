e954afac7ac7fc35d30d0e75664b6e2a6f66208f
Gguife <gguife747@gmail.com>
2024-03-08 19:54:36 -0300

    feat: Added verify operation route

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import coinsController from "../controllers/coins.js";
+import { verifyBusinessOperation } from "../controllers/businessOperation.js";
 
 const routes = express.Router();
 
@@ -8,5 +9,8 @@ routes.get('/coins/:id', coinsController.findOne);
 routes.post('/coins', coinsController.add);
 routes.put('/coins/:id', coinsController.update);
 routes.delete('/coins/:id', coinsController.remove);
+
+//Verificando moedas suportadas
+routes.post('/verify-operation', verifyBusinessOperation);
 
 export default routes;
