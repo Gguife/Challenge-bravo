a1aac95c91add674cd5fde4eb7b8b3e3ea589ca3
Gguife <gguife747@gmail.com>
2024-03-08 19:55:23 -0300

    refactor: Added betters features

--- src/controllers/businessOperation.js
+++ src/controllers/businessOperation.js
@@ -1,19 +1,32 @@
-import { coinsModel } from "../models/coinsModel";
-import getExchangeRates from "../api/currencyLayerApi";
+import { coinsModel } from "../models/coinsModel.js";
+import getExchangeRates from "../api/currencyLayerApi.js";
 
 export const verifyBusinessOperation = async (req, res) =>{
-  try{
-    const exchangeData = await getExchangeRates();
-   
-    //Obtendo códigos de moeda da API (code, codein)
+  try{  
+    // Obtendo moedas que queremos na solicitação
+    const { currency1, currency2 } = req.query;
+
+    // Verificando se as moedas realmente foram fornecidas
+    if (!currency1 || !currency2) {
+      return res.status(400).json({ error: 'Currency parameters missing from request.' });
+    }
+
+    // Obtendo dados da API
+    const exchangeData = await getExchangeRates(`${currency1}-${currency2}`);
+    // Verificar se há dados encontrados   
+    if (!exchangeData || exchangeData.length === 0) {
+      return res.status(404).json({ error: 'No exchange rate data found.' });
+    }
+
+    // Obtendo códigos de moeda da API (code, codein)
     const allCodesFromAPI = [...new Set([
       ...exchangeData.map(index => index.code),
       ...exchangeData.map(index => index.codein)
     ])];
 
-    //Fazendo a verificação se meu sistema suporta tal moeda
+    // Fazendo a verificação se meu sistema suporta tal moeda
     const supportedCoins = await coinsModel.findAll({
-      where: {currency_code: {$in: allCodesFromAPI}}
+      where: {currency_code:  allCodesFromAPI}
     });  
 
     //Retornando moedas suportadas
